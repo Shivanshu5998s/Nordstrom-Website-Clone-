99a2f86aebc566366bb8e57463efdaf598fd7cd3
Shivanshu5998s <96103252+Shivanshu5998s@users.noreply.github.com>
2022-06-18 11:39:57 +0530

    First Commit

---
+++ Nordstromrack-Website-Clone-main/src/App.js
@@ -0,0 +1,9 @@
+import Footer from "./Components/Footer/Footer";
+import React from "react";
+import MainRoutes from "./Routes/MainRoutes";
+
+function App() {
+  return (< MainRoutes />);
+}
+
+export default App;
