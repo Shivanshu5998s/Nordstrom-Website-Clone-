48189650f120a044523d41ded5a6069a2a18b1bf
Shivanshu5998s <96103252+Shivanshu5998s@users.noreply.github.com>
2022-06-22 16:27:40 +0530

    Adding some gift items on special days

---
+++ Nordstromrack-Website-Clone-main/src/Components/Gift/Gift.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Momgift, { Motherfaq, Therealdeal } from "./Momgift";
+import Shopyourway from "./Shopyourway";
+
+const Gift = () => {
+  return (
+    <>
+      <Momgift />
+      <Therealdeal />
+      <Motherfaq />
+      <Shopyourway />
+    </>
+  );
+};
+
+export default Gift;
