d38101846cd2f4c865246865a96d9d7d41c63767
Shivanshu5998s <96103252+Shivanshu5998s@users.noreply.github.com>
2022-06-25 18:01:20 +0530

    Add description page in inner pages

---
+++ Nordstromrack-Website-Clone-main/src/Components/InnerPages/Description/Description.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const Description = () => {
+
+  const data = useSelector((state) => state.Data.descriptionData);
+
+  console.log(data);
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Description;
